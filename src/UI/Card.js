f4684b37b559a29a9075c376490f65e347d48fe8
wilfried <wilfried.odorico@gmail.com>
2022-07-16 15:41:00 +0200

    ajust card to model

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
 import { colors } from '../theme/colors'
 
-const Card = ({ className, children, bgColor }) => {
-  return <div className={className} bgColor={bgColor}>{children}</div>
+const Card = ({ className, children, bgcolor }) => {
+  return <div className={className} bgcolor={bgcolor}>{children}</div>
 }
 
 export default styled(Card)`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  max-width: 450px;
+  max-width: 540px;
   width: 100%;
-  background-color: ${({ bgColor }) => bgColor || colors.softRed};
-  padding: 1.5rem;
-  margin: 1rem;
+  background-color: ${({ bgcolor }) => bgcolor || colors.softRed};
+  padding: 1.8rem 2rem;
+  margin-bottom: 1.4rem;
   border-radius: 1rem;
 `
